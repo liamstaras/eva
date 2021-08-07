fc7e584db9f9a86b7582bd6ea95e7c810913f46c
Liam Staras <liam.staras@gmail.com>
2021-08-07 14:59:24 +0100

    reorganize into classes

--- drawing.js
+++ drawing.js
@@ -1,15 +1,42 @@
 var c = document.getElementById("crossSection");
 var ctx = c.getContext("2d");
-function drawDisplay() {
-    ctx.beginPath();
-    ctx.arc(250,250,225,0,2*Math.PI);
-    ctx.closePath();
-    ctx.stroke();
-}
-function drawTrack(angle, length) {
+
+/*function drawTrack(angle, length) {
     ctx.moveTo(250,250);
     ctx.lineTo(250+length*Math.cos(angle),250-length*Math.sin(angle));
     ctx.stroke();
 }
 drawDisplay();
-drawTrack(-3.061, 150);
+drawTrack(-3.061, 150);*/
+
+class CrossSection {
+    constructor() {
+        this.canvas = document.getElementById("crossSection"); // bind the HTML element to the object *FUTURE: pass as parameter
+        this.context = this.canvas.getContext("2d"); // return a drawing frame object
+        this.drawAll(); // initial redraw
+    }
+    get dimensions() { // simple getter to give the (current) canvas dimensions
+        return [this.canvas.width,this.canvas.height]
+    }
+    get centre() { // simple getter to give the (current) geometric centre of the canvas
+        return [this.canvas.width/2,this.canvas.height/2]
+    }
+    drawAll() { // function to redraw all elements of the canvas
+        this.drawDisplay(); // redraw the display
+    }
+    drawDisplay() { // function to draw the display of the detector
+        this.context.beginPath(); // place our circle in a path
+        this.context.arc(
+            ...this.centre, // the centre of the circle is the centre of the canvas; unpack to x and y coord.s
+            Math.min(...this.dimensions)/2, // the diameter of the circle is half the shortest side length of the canvas
+            0, 2*Math.PI // draw an arc through 360 deg.
+            );
+        this.context.closePath(); // finish the path
+        this.context.stroke(); // actually draw on the canvas
+    }
+    drawTrack(angle, length) { // funcrion to plot a simple track in the display
+        this.context.moveTo(...this.centre); // move the cursor to the centre of the display
+        this.context.lineTo(this.centre[0]+length*Math.cos(angle),this.centre[1]-length*Math.sin(angle)); // plot a line at the desired angle
+        this.context.stroke(); // actually draw on the canvas
+    }
+}
---
+++ events.js
@@ -0,0 +1,6 @@
+class Track {
+    constructor(phi, length) {
+        this.phi = phi;
+        this.length = length;
+    }
+}
